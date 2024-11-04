bef331f8b013cfc9faa872bf2d14f2338a267488
Abhishek Raj <abhishekcultivates@gmail.com>
2024-11-04 20:30:29 +0530

    Updated Port & Origin for deployment

--- server/index.js
+++ server/index.js
@@ -12,12 +12,14 @@ import channelRoutes from "./routes/ChannelRoutes.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT;
-const databaseURL = process.env.DATABSE_URL;
+const port = process.env.PORT || 8747;
+const databaseURL = process.env.DATABASE_URL;
 
 app.use(
   cors({
-    origin: [process.env.ORIGIN],
+    origin: process.env.ORIGIN
+      ? [process.env.ORIGIN]
+      : ["http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
     credentials: true,
   })
